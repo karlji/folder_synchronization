20a4acd68f61293f528d5e422c7ad23e20de737f
Jiri Karlik <karlikjirka@gmail.com>
2024-10-02 16:00:41 +0200

    Docstrings updated. Clamp added

--- main.py
+++ main.py
@@ -35,23 +35,33 @@ class LinuxFolderSync(FolderSync):
     def sync(self):
         print(f"Syncing on Linux with source: {self.source} and replica: {self.replica}")
 
-def parse_arguments():
+def parse_arguments() -> argparse.Namespace:
     """
     Parses command-line arguments for source, replica, interval, and log file.
+
+    Parameters:
+
+    Returns:
+    - argparse.Namespace: Parsed arguments
     """
     parser = argparse.ArgumentParser(description='Sync folders between source and replica.')
 
     parser.add_argument('--source', type=str, required=True, help='Path to the source folder.')
     parser.add_argument('--replica', type=str, required=True, help='Path to the replica folder.')
     parser.add_argument('--log', type=str, required=True, help='Path to the log file.')
-    parser.add_argument('--interval', type=int, required=True, help='Sync interval in seconds.')
-
+    parser.add_argument('--interval', type=int, required=True, help='Sync interval in seconds. (1-86400)')
     return parser.parse_args()
 
 
-def check_path(path):
+def check_path(path: str) -> str:
     """
     Checks whether provided directory path exists. If not, asks user to create new folder.
+
+    Parameters:
+    - path (str): Path to the directory to check.
+
+    Returns:
+    - str: Path of existing directory.
     """
     path = path + "_new3"
     if not os.path.exists(path):
@@ -68,13 +78,27 @@ def check_path(path):
             break
     return path
 
+def clamp(n: int, minn: int, maxn: int) -> int:
+    """
+    Clamps a value to be within a specified range.
+
+    Parameters:
+    - n (int): The value to clamp.
+    - minn (int): Minimum allowed value.
+    - maxn (int): Maximum allowed value.
+
+    Returns:
+    - int: The clamped value.
+    """
+
 def main():
     args = parse_arguments()
 
+    #Checking parsed arguments
     source = check_path(args.source)
     replica = check_path(args.replica)
     log_file = check_path(args.log)
-    interval = args.interval
+    interval = clamp(args.interval,1,86400)
 
     # Detect the OS platform
     current_os = platform.system()
